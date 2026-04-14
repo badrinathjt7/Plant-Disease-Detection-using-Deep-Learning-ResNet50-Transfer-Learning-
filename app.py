a0d664dcd02c8566cd917dff7fbfa5a5f06e8080
Badrinath J.T <144896084+badrinathjt7@users.noreply.github.com>
2026-04-14 13:19:59 +0530

    Add plant disease detection interface with Gradio

---
+++ app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import preprocess_input
+
+IMG_SIZE = 96
+
+model = load_model("plant_disease_model.h5")
+
+class_names = [str(i) for i in range(38)]
+
+def predict(image):
+    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    image = preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+
+    preds = model.predict(image)[0]
+    idx = np.argmax(preds)
+
+    return f"Class {idx} ({preds[idx]*100:.2f}%)"
+
+interface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="numpy"),
+    outputs="text",
+    title="🌿 Plant Disease Detection",
+    description="Upload a leaf image to detect disease"
+)
+
+interface.launch()
